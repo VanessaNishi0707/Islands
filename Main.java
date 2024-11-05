8dc8a6c3bde84865905bb216c9b60577dcce2331
Moshir <alexandermoshir@gmail.com>
2024-11-04 18:34:51 -1000

    Separated out redundant methods between answers

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Testing Question 1...");
+        //Question1.main(args);
+        
+        System.out.println("Testing Question 4...");
+        Question4.main(args);
+    }
+}
